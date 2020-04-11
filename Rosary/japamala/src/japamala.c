9c4af1d161618c70b85926dc754cff6b6e692a6f
Antosebastian <kiranantosebastian@gmail.com>
2020-04-10 22:45:35 -0400

    04.10.2020

---
+++ Rosary/japamala/src/japamala.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name : rosary.c
+ Author : kiranantosebastian
+ Version :
+ Copyright : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+open()
+{
+	printf("Rosary\n");
+}
+
+void menu()
+{
+	int select;
+	open(); //this is the introduction screen
+	printf("1. Daily Rosary\n2. Today's mystery\n");
+	scanf("%d", &select);
+
+}
+
+int main()
+{
+	menu();
+	return 0;
+}
